723364235d7da203b8bff9c95b2bef5df781acc0
Andrew Rambidis <arambidis@gmail.com>
2023-11-26 16:18:52 -0500

    Added AIC, BIC for OLS regression; file structures.

---
+++ src/sim/ARMS.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy.typing as npt
+
+from .model_selection import OLS_AIC, OLS_BIC
+
+
+def ARMS(X: npt.NDArray[np.float32], Y: npt.NDArray[np.float32], m: np.int32):
+    pass
---
+++ src/sim/model_selection.py
@@ -0,0 +1,26 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def MSE(
+    Y_true: npt.NDArray[np.float32],
+    Y_pred: npt.NDArray[np.float32]
+        ) -> np.float32:
+    return (1 / Y_true.shape[0]) * np.linalg.norm(Y_true - Y_pred)**2
+
+
+def OLS_AIC(
+    Y_true: npt.NDArray[np.float32],
+    Y_pred: npt.NDArray[np.float32],
+    n: int,
+    p: int
+        ) -> list[float, float]:
+    return n * np.log(MSE(Y_true, Y_pred)) + 2 * p
+
+def OLS_BIC(
+    Y_true: npt.NDArray[np.float32],
+    Y_pred: npt.NDArray[np.float32],
+    n: int,
+    p: int
+        ) -> list[float, float]:
+    return n * np.log(MSE(Y_true, Y_pred)) + p * np.log(n)
